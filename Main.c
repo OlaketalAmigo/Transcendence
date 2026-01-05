62e030da141d7f5cc16d848eb0a14be998dc33fd
Thomas <144033880+OlaketalAmigo@users.noreply.github.com>
2026-01-05 14:34:03 +0100

    It begins !

---
+++ Main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Program received signal SIGSEGV, Segmentation Fault.\n__GI_raise (sig=sig@entry=6) at 0x54ffg67a ../sysdeps/unix/sysv/linux/c_balo.ken:666\nSee #845515 --api-fuck-you to get more information about it");
+  return 1;
+}
