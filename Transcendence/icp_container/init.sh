b272c3cb51dec8ec021c914cab1c9876b0787f0d
bitsearch <SNESS_1@proton.me>
2026-02-06 06:08:31 +0100

    add tetris: un new projet dfx sans git est creer, ce dossier sera utiliser comme volume comme l'a suggerer Anthony

--- Transcendence/icp_container/init.sh
+++ Transcendence/icp_container/init.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-#cette commande créer un nouveau projet dfx
-#les options --type et --frontend permettent
-#in finne d'enlever l'interaction de création
-#(sinon on ne peut pas automatiser et un humain doit-etre derriere)
-#mokoto pour le backend et sveltekit comme framework pour le front-end
+#ce script va lancer le projet icp_container en prenant les fichier du volume
 
-#je dois faire une condition pour les lancements suivants
-#il créera le projet s'il n'est pas deja la, sinon,
-#il passe directement aux autres commande
-
-if [ ! -d ./icp_container ]; then
-    dfx new icp_container --type motoko  --frontend sveltekit
-fi
-
-cd icp_container
+cd icp_container #chemin du volume
 dfx start --background --host 0.0.0.0:4943
 dfx deploy
 
