c549bbf66c4cda91a927319d96de0537f3f98a29
bitsearch <SNESS_1@proton.me>
2026-02-07 05:48:15 +0100

    modif: fichier retire, script init.sh et dockerfile modife

--- Transcendence/icp_container/init.sh
+++ Transcendence/icp_container/init.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 #ce script va lancer le projet icp_container en prenant les fichier du volume
 
+#create le projet
+dfx new tetris --type motoko --frontend sveltekit
+
 cd tetris #chemin du volume
-
-npm install # pour installer les dependancce npm au cas ou
-
-dfx cache install # pour installer les dependance motoko via dfx
-
-#telecharger les dependance des canister
-dfx deps pull
-dfx deps init
-dfx deps deploy
-
 
 dfx start --background --host 0.0.0.0:4943
 dfx deploy
